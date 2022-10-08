6662c9daa390b70295635ed7dcdabbf824772f48
Shipra Goswami <74846952+goswamishipra15@users.noreply.github.com>
2022-10-08 22:08:58 +0530

    Binary Tree Postorder Traversal
    Given the root of a binary tree, return the postorder traversal of its nodes' values.

---
+++ 28.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+ //NON-RECURSIVE SOLUTION
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.empty()){
+            root = stack.pop();
+            list.add(0,root.val);
+            if(root.left != null) stack.push(root.left);
+            if(root.right != null) stack.push(root.right);
+    }
+    return list;
+        
+    }
+}
+
+//RECURSIVE SOLUTION
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root == null) return list;
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
